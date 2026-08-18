2b1f233129fd74ff58ccd4be096d22df205309df
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 11:56:58 +0600

    Delete Break.java

--- Break.java
+++
@@ -1,9 +0,0 @@
-package generated;
-
-import generated.MiniLangException;
-
-/** LLM-generated reconstruction from the supplied description. */
-public class Break {
-    private final MiniLangException signal = new MiniLangException("break");
-public void execute() { throw signal; }
-}
