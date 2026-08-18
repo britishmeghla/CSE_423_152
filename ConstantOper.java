fd1585937cdd10045c35777c44bcc4a0d23fe489
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 12:03:19 +0600

    Delete ConstantOper.java

--- ConstantOper.java
+++
@@ -1,9 +0,0 @@
-package generated;
-
-
-/** LLM-generated reconstruction from the supplied description. */
-public class ConstantOper {
-    private final String constant;
-    public ConstantOper(String constant) { this.constant=constant; }
-public String execute() { return constant; }
-}
