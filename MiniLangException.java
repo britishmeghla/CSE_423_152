eae14b8b0ac2ac93afe433d0afa10eb90a1949cc
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 11:56:23 +0600

    Delete MiniLangException.java

--- MiniLangException.java
+++
@@ -1,8 +0,0 @@
-package generated;
-
-
-/** LLM-generated reconstruction from the supplied description. */
-public class MiniLangException extends RuntimeException {
-    public MiniLangException() { super(); }
-    public MiniLangException(String message) { super(message); }
-}
