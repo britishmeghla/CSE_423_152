0825de1a02d1b2eae358a45c6167a775a190481b
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 11:57:14 +0600

    Delete GenericCreateException.java

--- GenericCreateException.java
+++
@@ -1,9 +0,0 @@
-package generated;
-
-import generated.GenericEntityConfException;
-
-/** LLM-generated reconstruction from the supplied description. */
-public class GenericCreateException extends RuntimeException {
-    public GenericCreateException() { super(); }
-    public GenericCreateException(String message) { super(message); }
-}
