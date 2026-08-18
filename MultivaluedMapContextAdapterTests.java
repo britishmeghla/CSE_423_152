bf58789ad009d6a18b063ba3786f6c9722495060
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 11:53:59 +0600

    Delete MultivaluedMapContextAdapterTests.java

--- MultivaluedMapContextAdapterTests.java
+++
@@ -1,9 +0,0 @@
-package generated;
-
-import generated.ModelField;
-
-/** LLM-generated reconstruction from the supplied description. */
-public class MultivaluedMapContextAdapterTests {
-    private final ModelField field = new ModelField("test");
-public boolean runTest() { return field.getName().equals("test"); }
-}
