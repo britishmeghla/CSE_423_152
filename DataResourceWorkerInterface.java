89e60b9001941737c05f62609f06e374f3ebfd10
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 12:01:07 +0600

    Delete DataResourceWorkerInterface.java

--- DataResourceWorkerInterface.java
+++
@@ -1,6 +0,0 @@
-package generated;
-
-import generated.EntityTypeUtil;
-
-/** LLM-generated reconstruction from the supplied description. */
-public interface DataResourceWorkerInterface { String render(String resource); }
