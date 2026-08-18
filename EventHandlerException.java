88059ae6db20c1cc0fd6157f933a43d2f0292991
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 11:59:48 +0600

    Delete EventHandlerException.java

--- EventHandlerException.java
+++
@@ -1,8 +0,0 @@
-package generated;
-
-
-/** LLM-generated reconstruction from the supplied description. */
-public class EventHandlerException extends RuntimeException {
-    public EventHandlerException() { super(); }
-    public EventHandlerException(String message) { super(message); }
-}
