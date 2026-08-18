aef1fc155067c21ce49e568388ffdff4174ab2ca
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 11:36:55 +0600

    Delete ApacheFopWorker.java

--- ApacheFopWorker.java
+++
@@ -1,8 +0,0 @@
-package generated;
-
-import generated.ResourceLoader;
-
-/** LLM-generated reconstruction from the supplied description. */
-public class ApacheFopWorker {
-    public String render(String xml) { return "FORMATTED:" + xml; }
-}
