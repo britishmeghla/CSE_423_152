0dc126b64dbfd98f5edb08983c30e44d83a31401
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 11:55:32 +0600

    Delete ModelField.java

--- ModelField.java
+++
@@ -1,9 +0,0 @@
-package generated;
-
-
-/** LLM-generated reconstruction from the supplied description. */
-public class ModelField {
-    private final String name;
-    public ModelField(String name) { this.name=name; }
-    public String getName() { return name; }
-}
