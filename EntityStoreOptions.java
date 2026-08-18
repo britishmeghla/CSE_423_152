8c8bed5a708eaf858cda9d51a09bd35840a4ba48
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 11:58:53 +0600

    Delete EntityStoreOptions.java

--- EntityStoreOptions.java
+++
@@ -1,9 +0,0 @@
-package generated;
-
-
-/** LLM-generated reconstruction from the supplied description. */
-public class EntityStoreOptions {
-    private final boolean dummyForeignKeys;
-    public EntityStoreOptions(boolean dummyForeignKeys) { this.dummyForeignKeys=dummyForeignKeys; }
-    public boolean isDummyForeignKeys() { return dummyForeignKeys; }
-}
