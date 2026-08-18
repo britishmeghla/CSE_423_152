f90303a35f48c418f15ba91abbb66617282aa25e
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 11:58:36 +0600

    Delete FinAccountTest.java

--- FinAccountTest.java
+++
@@ -1,9 +0,0 @@
-package generated;
-
-import generated.EntityStoreOptions;
-
-/** LLM-generated reconstruction from the supplied description. */
-public class FinAccountTest {
-    private final EntityStoreOptions options = new EntityStoreOptions(false);
-public boolean runTest() { return !options.isDummyForeignKeys(); }
-}
