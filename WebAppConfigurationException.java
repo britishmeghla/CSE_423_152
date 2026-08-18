d9ccf66b0bc11e121bd92d050a64c9c191cf3abb
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 11:36:39 +0600

    Delete WebAppConfigurationException.java

--- WebAppConfigurationException.java
+++
@@ -1,8 +0,0 @@
-package generated;
-
-
-/** LLM-generated reconstruction from the supplied description. */
-public class WebAppConfigurationException extends RuntimeException {
-    public WebAppConfigurationException() { super(); }
-    public WebAppConfigurationException(String message) { super(message); }
-}
