53d3924260fe8ee53f8816e1ba4db673bf886b67
Meghla <2022-3-60-152@std.ewubd.edu>
2026-08-18 11:59:28 +0600

    Delete GenericEntityConfException.java

--- GenericEntityConfException.java
+++
@@ -1,8 +0,0 @@
-package generated;
-
-
-/** LLM-generated reconstruction from the supplied description. */
-public class GenericEntityConfException extends RuntimeException {
-    public GenericEntityConfException() { super(); }
-    public GenericEntityConfException(String message) { super(message); }
-}
